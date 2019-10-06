7f8bc4f91ad3309ff65ae64bae37c432fefe0913
Shani Almog <killerdrum@gmail.com>
2019-10-06 22:45:20 +0300

    add props and expansion card

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
-import React from 'react'
+import React, {Component} from "react"
 import Button from '@material-ui/core/Button'
 import './App.css'
+import WineItem from './components/WineItem/'
+import { Container } from '@material-ui/core'
 
-function App() {
-  return (
-    <div>
-      <Button variant="contained" color="primary">
-        Add wine
-      </Button>
-      <h1>
-        My wines
-      </h1>
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      wineList: [
+        {
+          id: 123,
+          name: "Old Tbilisi",
+          type: "Semi sweet red wine",
+          vintage: 2017,
+          cost: 15,
+          complexity: 1,
+          finish: 4,
+          typicity: 1
+        },        {
+          id: 123,
+          name: "Old Moldova",
+          type: "Semi sweet red wine",
+          vintage: 2017,
+          cost: 15,
+          complexity: 1,
+          finish: 4,
+          typicity: 1
+        },        {
+          id: 123,
+          name: "Old Ukraine",
+          type: "Semi sweet red wine",
+          vintage: 2017,
+          cost: 15,
+          complexity: 1,
+          finish: 4,
+          typicity: 1
+        }
+      ],
+      sortBy: "date"
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Container maxWidth="sm">
+          <h1>
+            My wines
+          </h1>
+          <Button variant="contained" color="primary">
+            Add wine
+          </Button>
+          <div>
+            {
+              this.state.wineList.map(item =>
+                <WineItem {...item}/>
+              )
+            }
+          </div>
+        </Container>
+      </div>
+    )
+  }
 }
+
+
 
 export default App
---
+++ src/components/WineItem/index.js
@@ -0,0 +1,42 @@
+import React, {Component} from "react"
+import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
+import Typography from '@material-ui/core/Typography'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+class WineItem extends Component {
+  render() {
+    const {
+      id,
+      name,
+      type,
+      vintage,
+      cost,
+      complexity,
+      finish,
+      typicity
+    } = this.props
+    return (
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{name}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    )
+  }
+}
+
+
+
+export default WineItem
